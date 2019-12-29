6b7956bc18a7e8a47c82c4d986f37c2550d7b953
gabybonaventura <gabybonaventura@hotmail.com>
2019-12-29 19:54:19 -0300

    Binding a simple reddit post

--- RedditUWP/Models/RedditPost.cs
+++ RedditUWP/Models/RedditPost.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RedditUWP.Models
 {
     class RedditPost
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime CreatedUTC { get; set; }
+        public string Thumbnail { get; set; }
+        public int NumComments { get; set; }
+        public bool ItWasRead { get; set; }
     }
 }
--- RedditUWP/ViewModels/MainViewModel.cs
+++ RedditUWP/ViewModels/MainViewModel.cs
@@ -1,20 +1,37 @@
-﻿using RedditUWP.ViewModels.Base;
+﻿using RedditUWP.Models;
+using RedditUWP.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace RedditUWP.ViewModels
 {
     class MainViewModel : BindableBase
     {
         #region Attributes
-        private int id;
+        private string id, title, author, thumbnail;
+        private ObservableCollection<RedditPost> posts;
         #endregion
         #region Constructors
         public MainViewModel()
         {
-            this.Id = 10;
+            this.Id = "10";
+            this.Thumbnail = "https://a.thumbs.redditmedia.com/NPbKE2FAaO97ci2jIVCy5L04nEWd60_nS52LdmMQMV4.jpg";
+            this.Author = "author";
+            this.Title = "title";
         }
         #endregion
         #region Properties
-        public int Id 
+        public ObservableCollection<RedditPost> Posts
+        {
+            get
+            {
+                return posts;
+            }
+            set
+            {
+                this.SetProperty(ref this.posts, value);
+            }
+        }
+        public string Id 
         {
             get
             {
@@ -23,6 +40,39 @@ namespace RedditUWP.ViewModels
             set
             {
                 this.SetProperty(ref this.id, value);
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                this.SetProperty(ref this.title, value);
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                this.SetProperty(ref this.author, value);
+            }
+        }
+        public string Thumbnail
+        {
+            get
+            {
+                return thumbnail;
+            }
+            set
+            {
+                this.SetProperty(ref this.thumbnail, value);
             }
         }
         #endregion
