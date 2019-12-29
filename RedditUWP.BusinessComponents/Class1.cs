b80fb53af55345aaaaef78db089f455f3f0bdbfa
gabybonaventura <gabybonaventura@hotmail.com>
2019-12-29 19:10:47 -0300

    n-layer architecture

---
+++ RedditUWP.BusinessComponents/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditUWP.BusinessComponents
+{
+    public class Class1
+    {
+    }
+}
